7fbf822f76f327ea522981de261590f906c3d146
TatyanaGV <gvtatyana@mail.ru>
2019-11-26 20:01:02 +0300

    Modified .appveyor.yml and LoginPage

--- src/test/java/ru/netology/web/page/LoginPage.java
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -1,14 +1,21 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.SelenideElement;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-  public VerificationPage validLogin(DataHelper.AuthInfo info) {
-    $("[data-test-id=login] input").setValue(info.getLogin());
-    $("[data-test-id=password] input").setValue(info.getPassword());
-    $("[data-test-id=action-login]").click();
-    return new VerificationPage();
-  }
+
+    private SelenideElement addLogin = $("[data-test-id=login] input");
+    private SelenideElement addPassword = $("[data-test-id=password] input");
+    private SelenideElement buttonLogin = $("[data-test-id=action-login]");
+
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        addLogin.setValue(info.getLogin());
+        addPassword.setValue(info.getPassword());
+        buttonLogin.click();
+        return new VerificationPage();
+    }
 }
